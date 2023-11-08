d426ff1afa717e3a9a2acfbbb14509cf17dfcaf7
i-sundermann <i.sundermann@zera.de>
2023-11-08 11:43:31 +0100

    tests: complete tests decodeHumidity add tests decodeAirPressure
    Signed-off-by: i-sundermann <i.sundermann@zera.de>

---
+++ tests/test_decodeairpressure.h
@@ -0,0 +1,17 @@
+#ifndef TEST_DECODEAIRPRESSURE_H
+#define TEST_DECODEAIRPRESSURE_H
+
+#include <QObject>
+
+class test_decodeAirPressure : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void test_500hPaAirPressure();
+    void test_minus500hPaAirPressure();
+
+
+};
+
+#endif // TEST_DECODEAIRPRESSURE_H
