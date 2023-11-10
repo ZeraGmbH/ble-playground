db4ba6c2b5f0674f62df459ee6fb5e8a829000f3
Andreas Müller <schnitzeltony@gmail.com>
2023-11-10 11:03:13 +0100

    Revert "BleDeviceDisoverer: Make sure deviceDiscovered connection is queued"
    This commit fixed nothing
    
    This reverts commit fecbace21b0be461c7d367372c7d97b001e3b6f6.

--- lib/bledevicedisoverer.cpp
+++ lib/bledevicedisoverer.cpp
@@ -2,9 +2,7 @@
 
 void BleDeviceDisoverer::start()
 {
-    connect(&m_deviceDiscoveryAgent, &QBluetoothDeviceDiscoveryAgent::deviceDiscovered,
-            this, &BleDeviceDisoverer::sigDeviceDiscovered,
-            Qt::QueuedConnection);
+    connect(&m_deviceDiscoveryAgent, &QBluetoothDeviceDiscoveryAgent::deviceDiscovered, this, &BleDeviceDisoverer::sigDeviceDiscovered);
     constexpr int permanentScanActive = 0;
     m_deviceDiscoveryAgent.setLowEnergyDiscoveryTimeout(permanentScanActive);
     m_deviceDiscoveryAgent.start(QBluetoothDeviceDiscoveryAgent::LowEnergyMethod);
