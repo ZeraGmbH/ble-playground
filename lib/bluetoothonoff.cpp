12b4a477ba1c5461c18a3f7690d811a91b5c128c
i-sundermann <i.sundermann@zera.de>
2023-11-13 14:34:17 +0100

    bluetoothonoff: new class to power on/off local bluetooth device
    Signed-off-by: i-sundermann <i.sundermann@zera.de>

---
+++ lib/bluetoothonoff.cpp
@@ -0,0 +1,27 @@
+#include "bluetoothonoff.h"
+
+BluetoothOnOff::BluetoothOnOff():
+    m_on(m_bluetoothDevice.hostMode() != QBluetoothLocalDevice::HostPoweredOff)
+{
+    connect(&m_bluetoothDevice, &QBluetoothLocalDevice::hostModeStateChanged,
+            this, &BluetoothOnOff::onHostModeChanged);
+}
+
+void BluetoothOnOff::switchOn(QBluetoothLocalDevice::HostMode hostMode)
+{
+    m_bluetoothDevice.setHostMode(hostMode);
+}
+
+void BluetoothOnOff::switchOff()
+{
+    m_bluetoothDevice.setHostMode(QBluetoothLocalDevice::HostPoweredOff);
+}
+
+void BluetoothOnOff::onHostModeChanged(QBluetoothLocalDevice::HostMode state)
+{
+    bool on = state != QBluetoothLocalDevice::HostPoweredOff;
+    if(m_on != on) {
+        m_on = on;
+        emit sigOnOff(on);
+    }
+}
