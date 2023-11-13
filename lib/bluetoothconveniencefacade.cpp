c6b11d206fe94043aa82687693f5164def884e14
i-sundermann <i.sundermann@zera.de>
2023-11-13 16:01:45 +0100

    Add BluetoothConvenienceFacade for convenience
    Signed-off-by: i-sundermann <i.sundermann@zera.de>

---
+++ lib/bluetoothconveniencefacade.cpp
@@ -0,0 +1,51 @@
+#include "bluetoothconveniencefacade.h"
+
+BluetoothConvenienceFacade::BluetoothConvenienceFacade()
+{
+    connect(&m_bluetoothOnOff, &BluetoothOnOff::sigOnOff,
+            this, &BluetoothConvenienceFacade::onOnOff);
+    connect(&m_bleDiscoverer, &BleDeviceDisoverer::sigDeviceDiscovered,
+            &m_bleDispatcher, &BleDeviceInfoDispatcher::onDeviceDiscovered);
+}
+
+void BluetoothConvenienceFacade::restartDiscoverer()
+{
+    m_bleDiscoverer.stop();
+    m_bleDiscoverer.start();
+}
+
+void BluetoothConvenienceFacade::start()
+{
+    if(m_bluetoothOnOff.isOn())
+        restartDiscoverer();
+    else
+        m_bluetoothOnOff.switchOn();
+}
+
+void BluetoothConvenienceFacade::stop()
+{
+    m_bluetoothOnOff.switchOff();
+}
+
+BleDispatcherId BluetoothConvenienceFacade::addBleDecoder(BluetoothDeviceInfoDecoderPtr decoder)
+{
+    return m_bleDispatcher.addBleDecoder(decoder);
+}
+
+BluetoothDeviceInfoDecoderPtr BluetoothConvenienceFacade::removeBleDecoder(BleDispatcherId idReturnedOnAdd)
+{
+    return m_bleDispatcher.removeBleDecoder(idReturnedOnAdd);
+}
+
+BluetoothDeviceInfoDecoderPtr BluetoothConvenienceFacade::findBleDecoder(BleDispatcherId idReturnedOnAdd)
+{
+    return m_bleDispatcher.findBleDecoder(idReturnedOnAdd);
+}
+
+void BluetoothConvenienceFacade::onOnOff(bool on)
+{
+    if(on)
+        restartDiscoverer();
+    else
+        m_bleDiscoverer.stop();
+}
---
+++ lib/bluetoothconveniencefacade.h
@@ -0,0 +1,30 @@
+#ifndef BLUETOOTHCONVENIENCEFACADE_H
+#define BLUETOOTHCONVENIENCEFACADE_H
+
+#include "bledeviceinfodispatcher.h"
+#include "bluetoothonoff.h"
+#include "bledevicedisoverer.h"
+
+class BluetoothConvenienceFacade : public QObject
+{
+    Q_OBJECT
+public:
+    BluetoothConvenienceFacade();
+    void start();
+    void stop();
+    BleDispatcherId addBleDecoder(BluetoothDeviceInfoDecoderPtr decoder);
+    BluetoothDeviceInfoDecoderPtr removeBleDecoder(BleDispatcherId idReturnedOnAdd);
+    BluetoothDeviceInfoDecoderPtr findBleDecoder(BleDispatcherId idReturnedOnAdd);
+signals:
+    void sigOnOff(bool on);
+
+private slots:
+    void onOnOff(bool on);
+private:
+    void restartDiscoverer();
+    BluetoothOnOff m_bluetoothOnOff;
+    BleDeviceDisoverer m_bleDiscoverer;
+    BleDeviceInfoDispatcher m_bleDispatcher;
+};
+
+#endif // BLUETOOTHCONVENIENCEFACADE_H
